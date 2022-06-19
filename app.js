5da0534223752186f25182626fb73c237221649c
Preyash-tryin-to-code <preyashyadav29@gmail.com>
2022-06-19 08:09:18 +0530

    blogging application is ready

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-
+const _ = require("lodash");
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent =
@@ -50,11 +50,17 @@ app.post("/compose", function (req, res) {
 });
 
 app.get("/posts/:postName", function (req, res) {
-  const requstedTopic = req.params.postName;
+  const requestedTopic = req.params.postName;
+  const fixRequestedTopic = _.lowerCase(requestedTopic);
   posts.forEach(function (post) {
     const storedTopic = post.title;
-    if (requstedTopic === storedTopic) {
-      console.log("MATCH FOUND!");
+    const fixStoredTopic = _.lowerCase(storedTopic);
+    if (fixRequestedTopic === fixStoredTopic) {
+      // console.log("MATCH FOUND!");
+      res.render("post", {
+        separatePostTitle: post.title,
+        separatePostContent: post.content,
+      });
     } else {
       console.log("NO BLOGS ON THIS TOPIC :/ ");
     }
